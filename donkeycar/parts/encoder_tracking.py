f8d32079c7e7cfdc2857a6bac7d999c38f0c68be
ThatRadSeal <108237185+ThatRadSeal@users.noreply.github.com>
2023-07-19 16:24:43 -0500

    Update encoder_tracking.py

--- donkeycar/parts/encoder_tracking.py
+++ donkeycar/parts/encoder_tracking.py
@@ -1,4 +1,18 @@
 import queue
 
-# queue to keep track of car velocities read from the encoder
-velocity_queue = queue.Queue()
+class VelocityQueue:
+
+  def __init__(self):
+    # queue to keep track of car velocities read from the encoder
+    self.queue = queue.Queue()
+
+  # return the next item in queue without removing
+  def top(self):
+    if not self.queue.empty():
+      velocity = self.queue.get()
+      self.queue.put(velocity)
+      return velocity
+    else:
+      return 0
+
+velocity_queue = VelocityQueue()
