4964e7e4ad48be56774a6501cc2437a8afa7d0c2
ThatRadSeal <108237185+ThatRadSeal@users.noreply.github.com>
2023-07-26 13:37:32 -0500

    Update vehicle.py

--- donkeycar/vehicle.py
+++ donkeycar/vehicle.py
@@ -60,7 +60,7 @@ class PartProfiler:
 
 
 class Vehicle:
-    def __init__(self, max_speed, mem=None):
+    def __init__(self, max_speed=None, mem=None):
 
         if not mem:
             mem = Memory()
@@ -72,7 +72,7 @@ class Vehicle:
 
         #add these for keeping track of speed
         self.constant_speed = max_speed
-        self.prev_speed = []
+        self.prev_speed = [0]
         self.speed_data = open("speed_data.txt", "a") # used for graphing speed
         self.speed_data.write("Record of enc/speed values:\n")
 
@@ -211,17 +211,19 @@ class Vehicle:
                 inputs = self.mem.get(entry['inputs'])
 
                 # change PWM Throttle if too low of a speed
-                if p.__class__.__name__ == "PWMThrottle":   
-                    if inputs == [1]:
-                        if len(self.prev_speed) >= 100:
-                            curr_speed = self.average_speed()
-                            self.prev_speed = []
-                            if curr_speed < (self.constant_speed - (0.1 * self.constant_speed)):
-                                p.max_pulse += 1
-                            if curr_speed > (self.constant_speed + (0.1 * self.constant_speed)):
-                                p.max_pulse -= 1
-                            print(f"recorded average speed: ", curr_speed)
-                            print(f"new PWM value: ", p.max_pulse)
+                if self.constant_speed is not None:
+                    if p.__class__.__name__ == "PWMThrottle":   
+                        if inputs == [1]:
+                            if len(self.prev_speed) >= 100:
+                                curr_speed = self.average_speed()
+                                self.speed_data.write(str(curr_speed) + "\n")
+                                self.prev_speed = []
+                                if curr_speed < (self.constant_speed - (0.1 * self.constant_speed)):
+                                    p.max_pulse += 1
+                                if curr_speed > (self.constant_speed + (0.1 * self.constant_speed)):
+                                    p.max_pulse -= 1
+                                print(f"recorded average speed: ", curr_speed)
+                                print(f"new PWM value: ", p.max_pulse)
 
                 # run the part
                 if entry.get('thread'):
@@ -236,7 +238,6 @@ class Vehicle:
                     if p.__class__.__name__ == "BicyclePose":
                         # write speed data if full throttle
                         if inputs[0] == 1:
-                            self.speed_data.write(str(self.prev_speed) + "\n")
                             self.prev_speed.append(outputs[1])
 
                 # finish timing part run
